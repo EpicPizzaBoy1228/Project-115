53ec1d4d2de694b7230313686bb238c543c80b2a
EpicPizzaBoy1228 <82849286+EpicPizzaBoy1228@users.noreply.github.com>
2021-09-27 18:42:41 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function preload() {
+    
+  }
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+function draw() {
+  fill(2,55,0)
+  stroke(2,55,0)
+  circle(22,22,22)
+}
